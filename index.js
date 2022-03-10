9b459b9df4f0e2c499fe84dc45ced7792d593fe4
John Leszczynski <jrleszczynski@gmail.com>
2022-03-09 21:59:21 -0800

    Add param for specifying maxFrequency in results

--- index.js
+++ index.js
@@ -4,19 +4,26 @@
  * @param {{
  *     sampleTimeLength: number,
  *     fftSize: number,
+ *     maxFrequency: number,
  *     }=} options
  *           sampleTimeLength Interval in seconds at which to start each sample.
  *               The duration of the audio file / sampleTimeLength = the number
  *               of samples.
  *           fftSize Integer, representing the window size of the FFT, given in
  *               number of samples. Must be a power of 2 between 2^5 and 2^15.
+ *           maxFrequency Maximum frequency in hz to include in the results.
+ *               Actual max will be
+ *               min(audioFile sample rate / 2, maxFrequency).
  * @return {!Promise<!Array<!Uint8Array>>}  an array of frequency samples;
  *     each sample is a normalized array of decibel values between 0 and 255.
  *     The frequencies are spread linearly from 0 to 1/2 of the sample rate.
  */
 async function getAudioFrequencyData(audioFile,
-    {sampleTimeLength = 1/60,
-      fftSize = 2 ** 11} = {}) {
+    {
+      sampleTimeLength = 1/60,
+      fftSize = 2 ** 11,
+      maxFrequency = 44100 / 2,
+    } = {}) {
   const audioContext = new AudioContext();
   return fetch(audioFile).then((response) => response.arrayBuffer())
       .then(async (buffer) => {
@@ -31,13 +38,18 @@ async function getAudioFrequencyData(audioFile,
 
         const analyser = offlineContext.createAnalyser();
         audioBufferSource.connect(analyser);
+        // Math.floor((decodedData.sampleRate / 2) /
         const numSamples = Math.floor(
             audioBufferSource.buffer.duration / sampleTimeLength);
         analyser.fftSize = fftSize;
 
         // Prep frequenyData array
         const frequencyData = new Array(numSamples);
-        const frequencyBinCount = analyser.frequencyBinCount;
+        const frequencyBandSize = (decodedData.sampleRate / 2) /
+          analyser.frequencyBinCount;
+        const frequencyBinCount = Math.min(
+            analyser.frequencyBinCount,
+            Math.max(maxFrequency / frequencyBandSize));
         for (let i = 0; i < numSamples; i++) {
           frequencyData[i] = new Uint8Array(frequencyBinCount);
         }
