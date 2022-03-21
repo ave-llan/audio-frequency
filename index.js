ab32e7046f95788055c1400b03933c8aba47094b
John Leszczynski <jrleszczynski@gmail.com>
2022-03-21 07:31:52 -0700

    Add get audioFrequency to class

--- index.js
+++ index.js
@@ -22,6 +22,74 @@ class AudioData {
     const audioBuffer = await audioContext.decodeAudioData(arrayBuffer)
     return new AudioData(audioBuffer)
   }
+
+  /**
+   * Returns audio frequency data for the given audio file.
+   * @param {{
+   *     sampleTimeLength: number,
+   *     fftSize: number,
+   *     maxFrequency: number,
+   *     }=} options
+   *           sampleTimeLength Interval in seconds at which to start each sample.
+   *               The duration of the audio file / sampleTimeLength = the number
+   *               of samples.
+   *           fftSize Integer, representing the window size of the FFT, given in
+   *               number of samples. Must be a power of 2 between 2^5 and 2^15.
+   *           maxFrequency Maximum frequency in hz to include in the results.
+   *               Actual max will be
+   *               min(audioFile sample rate / 2, maxFrequency).
+   *           smoothingTimeConstant A value from 0 -> 1 where 0 represents no
+   *               time averaging with the last analysis frame.
+   * @return {!Promise<!Array<!Uint8Array>>}  an array of frequency samples;
+   *     each sample is a normalized array of decibel values between 0 and 255.
+   *     The frequencies are spread linearly from 0 to 1/2 of the sample rate.
+   */
+  getFrequencyData({
+    sampleTimeLength = 1/60,
+    fftSize = 2 ** 11,
+    maxFrequency = 44100 / 2,
+    smoothingTimeConstant = 0.5,
+  } = {}) {
+    const offlineContext = new OfflineAudioContext(
+      this.numberOfChannels,
+      this.length,
+      this.sampleRate)
+    const audioBufferSource = offlineContext.createBufferSource()
+    audioBufferSource.buffer = this.buffer
+
+    const analyser = offlineContext.createAnalyser()
+    audioBufferSource.connect(analyser)
+    // Math.floor((audioData.sampleRate / 2) /
+    const numSamples = Math.floor(
+      audioBufferSource.buffer.duration / sampleTimeLength)
+    analyser.fftSize = fftSize
+    analyser.smoothingTimeConstant = smoothingTimeConstant
+
+    // Prep frequenyData array
+    const frequencyData = new Array(numSamples)
+    const frequencyBandSize = (this.sampleRate / 2) /
+      analyser.frequencyBinCount
+    const frequencyBinCount = Math.min(
+      analyser.frequencyBinCount,
+      Math.max(maxFrequency / frequencyBandSize))
+    for (let i = 0; i < numSamples; i++) {
+      frequencyData[i] = new Uint8Array(frequencyBinCount)
+    }
+    return new Promise((resolve) => {
+      for (let frameIndex = 0; frameIndex < numSamples; frameIndex++) {
+        offlineContext.suspend(sampleTimeLength * frameIndex).then(() => {
+          analyser.getByteFrequencyData(frequencyData[frameIndex])
+          offlineContext.resume()
+          // After populating last data, resolve promise.
+          if (frameIndex + 1 === numSamples) {
+            resolve(frequencyData)
+          }
+        })
+      }
+      offlineContext.startRendering()
+      audioBufferSource.start()
+    })  
+  }
 }
 
 /**
@@ -54,46 +122,7 @@ async function getAudioFrequencyData(audioFile,
     smoothingTimeConstant = 0.5,
   } = {}) {
   const audioData = await AudioData.fromFile(audioFile)
-  const offlineContext = new OfflineAudioContext(
-    audioData.numberOfChannels,
-    audioData.length,
-    audioData.sampleRate)
-  const audioBufferSource = offlineContext.createBufferSource()
-  audioBufferSource.buffer = audioData.buffer
-
-  const analyser = offlineContext.createAnalyser()
-  audioBufferSource.connect(analyser)
-  // Math.floor((audioData.sampleRate / 2) /
-  const numSamples = Math.floor(
-    audioBufferSource.buffer.duration / sampleTimeLength)
-  analyser.fftSize = fftSize
-  analyser.smoothingTimeConstant = smoothingTimeConstant
-
-  // Prep frequenyData array
-  const frequencyData = new Array(numSamples)
-  const frequencyBandSize = (audioData.sampleRate / 2) /
-      analyser.frequencyBinCount
-  const frequencyBinCount = Math.min(
-    analyser.frequencyBinCount,
-    Math.max(maxFrequency / frequencyBandSize))
-  for (let i = 0; i < numSamples; i++) {
-    frequencyData[i] = new Uint8Array(frequencyBinCount)
-  }
-  return new Promise((resolve) => {
-    for (let frameIndex = 0; frameIndex < numSamples; frameIndex++) {
-      offlineContext.suspend(sampleTimeLength * frameIndex).then(() => {
-        analyser.getByteFrequencyData(frequencyData[frameIndex])
-        offlineContext.resume()
-        // After populating last data, resolve promise.
-        if (frameIndex + 1 === numSamples) {
-          resolve(frequencyData)
-        }
-      })
-    }
-    offlineContext.startRendering()
-    audioBufferSource.start()
-  })  
-
+  return audioData.getFrequencyData({sampleTimeLength, fftSize, maxFrequency, smoothingTimeConstant})
 }
 
 export {getAudioFrequencyData}
